51d971f7671ab85470046a35bb1e8466433cb81f
Esteda <kim.karina@list.ru>
2019-03-08 00:19:44 +0600

    connected front to back

---
+++ build/precache-manifest.d0c068c747dc9bc03597d2ab93e204ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "07b211641bc27aab0acadb2a7723669a",
+    "url": "/static/media/avatar.07b21164.jpg"
+  },
+  {
+    "revision": "fe23131de62f8f1679ae",
+    "url": "/static/css/main.f9a23fa2.chunk.css"
+  },
+  {
+    "revision": "521b447e96281b81c5763f5206249500",
+    "url": "/static/media/back1.521b447e.jpg"
+  },
+  {
+    "revision": "6e3683e0ab87a9c4c77e",
+    "url": "/static/js/1.6e3683e0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d60a13748f155ffc4fc5bb14c312c8dd",
+    "url": "/static/media/photo.d60a1374.jpg"
+  },
+  {
+    "revision": "fe23131de62f8f1679ae",
+    "url": "/static/js/main.fe23131d.chunk.js"
+  },
+  {
+    "revision": "2744b0ad3fcd235fc4f63d85182064fa",
+    "url": "/static/media/icon.2744b0ad.png"
+  },
+  {
+    "revision": "ca5fccf9fab482561ee30145dda29d79",
+    "url": "/static/media/gif.ca5fccf9.gif"
+  },
+  {
+    "revision": "e1b5f42216afc61c9e0f80ebc465de5f",
+    "url": "/static/media/NEW.e1b5f422.gif"
+  },
+  {
+    "revision": "6e3683e0ab87a9c4c77e",
+    "url": "/static/css/1.8f4ee77c.chunk.css"
+  },
+  {
+    "revision": "d1bb3d56eefe574dee1a39db28f9cbb1",
+    "url": "/index.html"
+  }
+];
