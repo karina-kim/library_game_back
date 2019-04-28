d640281a1938fa96680606ff427305513f6d7b61
Esteda <kim.karina@list.ru>
2019-04-28 22:29:52 +0600

    fixed game

--- server/Routes and controllers/Responces.js
+++ server/Routes and controllers/Responces.js
@@ -2,10 +2,10 @@ module.exports.send_errors = function(errors,res)
 {
 	console.log("ERR"+errors);
 	if(errors.message){	
-	res.status(200).send(JSON.stringify({"errors":[errors.message]}));
+	res.status(500).send(JSON.stringify({"errors":[errors.message]}));
 	}
 
-	else res.status(200).send(JSON.stringify({"errors":errors}));
+	else res.status(500).send(JSON.stringify({"errors":errors}));
 }
 module.exports.send_responce = function (responce, res)
 {
